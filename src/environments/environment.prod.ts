d95326e99d3099e86b643fc9f8e6b2e006ed4174
Leroy Salih <Leroy.Salih@gmail.com>
2018-11-15 19:16:43 +0300

    DeployPoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDjvAbZ7_WyrVLGZQp3LrLIhSNFkj8VXhU',
-    authDomain: 'insight-e1d4e.firebaseapp.com',
-    databaseURL: 'https://insight-e1d4e.firebaseio.com',
-    projectId: 'insight-e1d4e',
-    storageBucket: 'insight-e1d4e.appspot.com',
-    messagingSenderId: '734599135461',
+    apiKey:             'AIzaSyD6ozOdXu5_F3i6rdntm2v7OmCa_roBTRI',
+    authDomain:         'insight-e1d4e.firebaseapp.com',
+    databaseURL:        'https://insight-e1d4e.firebaseio.com',
+    projectId:          'insight-e1d4e',
+    storageBucket:      'insight-e1d4e.appspot.com',
+    messagingSenderId:  '734599135461',
     timestampsInSnapshots: true,
   }
 };
